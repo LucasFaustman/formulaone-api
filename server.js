3a2a998fb6f615cec970170f7fc9e4f3cf922e9b
Lucas Faustman <lucasfaustman@gmail.com>
2022-06-05 13:41:45 -0700

    Added CORS

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
 class Driver {
     constructor(name,nationality,team,image,nickname = 'No nickname :(') {
